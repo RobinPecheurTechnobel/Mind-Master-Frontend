1979f6c7f98ccbcbc4f025af85683e5f241e1cbe
RobinPecheurTechnobel <robin.pecheur@edu.technobel.be>
2023-12-22 13:37:59 +0100

    token interceptor

---
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    let token = localStorage.getItem("MindToken");
+    if(token && token.replaceAll(" ","") != "")
+    {
+      let newRequest : HttpRequest<unknown> = request.clone({setHeaders : { 'Authorization' : `Bearer ${token}`}} );
+
+      return next.handle(newRequest);
+    }
+    return next.handle(request);
+  }
+}
